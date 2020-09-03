4f577ea18659733724fb6d67af1cfadeeb18c104
varunraj <varunraj@CORP.AMAZONWORKSPACES.COM>
2020-09-03 13:35:13 -0400

    initial commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home =()=>{
+    return (
+        <div>
+            I am the home component
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+//react imports
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+
+
+app.get('/',(req,res)=>{
+    const content = renderToString(<Home />);
+    res.send(content);
+})
+
+app.listen(3000, ()=>{
+    console.log('Listening to 3000')
+})
