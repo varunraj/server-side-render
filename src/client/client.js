859fd64e3d1aebf3eb927ca453f0bf3ff1049c8e
varunraj@CORP.AMAZONWORKSPACES.COM <varunraj@CORP.AMAZONWORKSPACES.COM>
2020-09-07 11:31:47 -0400

    completed user list

--- src/client/client.js
+++ src/client/client.js
@@ -1,16 +1,28 @@
 // Startup point for client side code
 
+//below code is to take care async calls inside actions creators
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import Routes from './Routes'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
 
 // we are redendering to the same div from index.js
 // so that react will enrich dom with all additinal js.
 
 ReactDOM.hydrate(
-    <BrowserRouter>
-         <Routes />
-    </BrowserRouter>,
+     <Provider store={store}>
+          <BrowserRouter>
+               <Routes />
+          </BrowserRouter>
+     </Provider>,
      document.querySelector("#root")
 );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+//below code is to take care async calls inside actions creators
+import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore';
 
 
 const app = express();
 app.use(express.static('public'));
 
 
-app.get('*',(req,res)=>{
+app.get('*', (req,res)=>{
+    
+    // logic to add logic and data to store
+
+    const store = createStore();
     //we pass req object to render so that it can be 
     // passed to static router to extract url
-    res.send(renderer(req));
+    
+    res.send(renderer(req, store));
 })
 
 
